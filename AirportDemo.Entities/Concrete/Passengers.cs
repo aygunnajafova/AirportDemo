d11a19b9c682a3c11090f077706d51d4f7459dc5
Aygün Nəcəfova <aygun.nacafova.577@teams.dia.edu.az>
2021-05-11 11:26:19 +0400

    AirportProject

---
+++ AirportDemo.Entities/Concrete/Passengers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirportDemo.Entities.Concrete
+{
+    class Passengers
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int AircraftId { get; set; }
+        public int FlightId { get; set; }
+    }
+}
